b9405c58104ca2790bbe4a7c01bbbb6b5ddf2846
arturo <ahpareja@gmail.com>
2022-08-10 16:36:24 -0500

    Fix Like model bug

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,6 @@ class Like < ApplicationRecord
   belongs_to :post
 
   def self.update_like_counter post
-    post.update(likeCount: Like.where(post: post).count)
+    post.update(likeCounter: Like.where(post: post).count)
   end
 end
