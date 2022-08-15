9d93a0534feec9fdff183dbaef333245fa21895e
arturo <ahpareja@gmail.com>
2022-08-15 12:13:39 -0500

    bug fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,5 @@
 class UsersController < ApplicationController
-  def home
-    @users = User.all
-  end
+  def home; end
 
-  def show
-    @user = User.find(params[:id])
-  end
+  def show; end
 end
