202bf2577dcf601d5d7e8f9e32625720b61469a8
arturo <ahpareja@gmail.com>
2022-08-17 17:13:28 -0500

    Add post show page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,5 +4,8 @@ class PostsController < ApplicationController
     @posts = @user.posts
   end
 
-  def show; end
+  def show
+    @post = Post.find(params[:id])
+    @comments = @post.comments
+  end
 end
