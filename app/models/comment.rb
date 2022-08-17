d34fc617a3dde0725af2306074f916648a2df61f
arturo <ahpareja@gmail.com>
2022-08-17 12:41:31 -0500

    Create call back for counters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  after_create :update_comment_counter
+
   def update_comment_counter
     post.commentsCount = post.commentsCount + 1
     post.save
