450d7109daacb82c891f273c9640a6722ab839d9
arturo <ahpareja@gmail.com>
2022-08-19 11:07:56 -0500

    Add redirect for like buttons

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,6 +4,6 @@ class LikesController < ApplicationController
     @post = Post.find(params[:post_id])
     @author = User.find(params[:user_id])
     Like.create(user: @user, post: @post)
-    redirect_to user_posts_path(@author)
+    redirect_to request.referrer
   end
 end
