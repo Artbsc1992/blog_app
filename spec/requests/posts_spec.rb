c2a0ae7de0e6971db13c4aaa859d8310703e31e4
arturo <ahpareja@gmail.com>
2022-08-23 11:14:50 -0500

    Correct posts require tests

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe 'PostsController', type: :request do
   describe 'GET users/:id/posts' do
     before do
-      get user_posts_path(user_id: '10')
+      user = User.new(name: 'Tom', bio: 'I am a developer', photo: 'photo.png', postCount: 5)
+      user.save
+      get user_posts_path(user_id: '1')
     end
 
     it 'should return an HTTP status 200 (ok)' do
@@ -14,14 +16,18 @@ RSpec.describe 'PostsController', type: :request do
       expect(response).to render_template(:index)
     end
 
-    it "Body should include the text 'Posts list'" do
-      expect(response.body).to include('Posts list')
+    it "Body should include the text 'User name'" do
+      expect(response.body).to include('Tom')
     end
   end
 
   describe 'GET users/:id/posts/:id' do
     before do
-      get user_post_path(user_id: '10', id: '10')
+      user = User.new(name: 'Tom', bio: 'I am a developer', photo: 'photo.png', postCount: 5)
+      user.save
+      post = Post.new(user: user, title: 'Post title', text: 'Post text')
+      post.save
+      get user_post_path(user_id: user.id, id: post.id)
     end
 
     it 'should return an HTTP status 200 (ok)' do
@@ -32,8 +38,8 @@ RSpec.describe 'PostsController', type: :request do
       expect(response).to render_template(:show)
     end
 
-    it "Body should include the text 'Post 1'" do
-      expect(response.body).to include('Post 1')
+    it "Body should include the text 'Post text''" do
+      expect(response.body).to include('Post text')
     end
   end
 end
