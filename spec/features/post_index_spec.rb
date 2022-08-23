8d2c0783e63220e3d871595794b4202fbee3a40b
Trictonicmp <code_shadow@hotmail.com>
2022-08-23 14:00:24 -0500

    Add integration test for post comments

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -20,32 +20,19 @@ RSpec.describe 'Post', type: :system do
     visit('http://localhost:3000/users/1/posts/11')
     expect(page).to have_content('Likes: 1') 
   end
-=begin 
-
+  
   it 'shows posts body' do
-    visit('http://localhost:3000/users/136/posts/140')
-    expect(page).to have_content('Lorem ipsum')
+    visit('http://localhost:3000/users/1/posts/11')
+    expect(page).to have_content('This is my first post!')
   end
-
+  
   it 'shows the names of each commentor' do
-    visit('http://localhost:3000/users/136/posts/140')
+    visit('http://localhost:3000/users/1/posts/11')
     expect(page).to have_content('Tom')
-    expect(page).to have_content('Josh')
   end
-
   it 'shows the comments the post has' do
-    visit('http://localhost:3000/users/136/posts/140')
-    expect(page).to have_content('this is my sixth comment six')
-    expect(page).to have_content('this is my fifth comment five')
-    expect(page).to have_content('this is my fourth comment four')
-    expect(page).to have_content('this is my third comment three')
-    expect(page).to have_content('this is my second comment two')
-    expect(page).to have_content('this is my first comment one')
-    expect(page).to have_content('nice post')
-    expect(page).to have_content('other comment')
-    expect(page).to have_content('ab')
-    expect(page).to have_content('dd')
-    expect(page).to have_content('Hey you!')
+    visit('http://localhost:3000/users/1/posts/11')
+    expect(page).to have_content('Hello')
+    expect(page).to have_content('Hello Hello')
   end 
-=end
 end
